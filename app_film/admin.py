960bd31606f0e600fdfd223ef59e687b094f875e
Dembo esther <demboesther674@gmail.com>
2024-02-28 15:28:56 +0100

    frr

--- app_film/admin.py
+++ app_film/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from app_film.model film import * 
 from app_film.model genre import *
+
 # Register your models here.
+admin.site.Register(film)
+admin.site.Register(genre)
